2dbb771c559b8c65c0a714394122999e8e609884
Saif Maamun <sadifmamun@gmail.com>
2025-02-09 09:36:45 +0600

    function

--- function test 2.py
+++ function test 2.py
@@ -13,3 +13,15 @@ def max3(a,b,c):
 
 x= max3(94,9,56)
 print(x)
+
+# another way
+def max3a(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+
+x= max3a(94,9,56)
+print(x)
